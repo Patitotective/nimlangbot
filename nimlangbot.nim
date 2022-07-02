13e42adbe24598f10340abe2e2839763d16510f7
Patitotective <cristobalriaga@gmail.com>
2022-07-02 18:42:15 -0500

    Now echoing found tweets

--- nimlangbot.nim
+++ nimlangbot.nim
@@ -9,6 +9,9 @@ proc main() =
   let twitterAPI = newTwitterAPI(getEnv("CONSUMER_KEY"), getEnv("CONSUMER_SECRET"), getEnv("ACCESS_TOKEN"), getEnv("ACCESS_TOKEN_SECRET"))
   let data = twitterApi.tweetsSearchRecent("#nimlang OR #nim_lang OR nimlang OR nim_lang lang:en -malware -virus -is:retweet").body.parseJson()["data"]
 
+  echo "Found the following tweets: "
+  echo data.pretty()
+
   for tweet in data:
     discard twitterApi.usersIdRetweets(botId, %* {"tweet_id": tweet["id"]})
     discard twitterApi.usersIdLikes(botId, %* {"tweet_id": tweet["id"]})
