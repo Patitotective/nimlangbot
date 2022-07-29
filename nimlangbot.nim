535ca7c449ad617d9d22299e004f191576d5964b
Patitotective <cristobalriaga@gmail.com>
2022-07-29 17:53:37 -0500

    Now matching "nim language" literally

--- nimlangbot.nim
+++ nimlangbot.nim
@@ -7,7 +7,7 @@ proc main() =
   assert existsEnv("CONSUMER_KEY") and existsEnv("CONSUMER_SECRET") and existsEnv("ACCESS_TOKEN") and existsEnv("ACCESS_TOKEN_SECRET")
 
   let twitterAPI = newTwitterAPI(getEnv("CONSUMER_KEY"), getEnv("CONSUMER_SECRET"), getEnv("ACCESS_TOKEN"), getEnv("ACCESS_TOKEN_SECRET"))
-  let data = twitterApi.tweetsSearchRecent("(#nimlang OR nimlang OR nim_lang OR (nim language) OR from:nim_lang OR retweets_of:nim_lang) lang:en -malware -virus -is:retweet -is:reply", {"max_results": "50"}.newStringTable).body.parseJson()["data"]
+  let data = twitterApi.tweetsSearchRecent("(#nimlang OR nimlang OR nim_lang OR \"nim language\" OR from:nim_lang OR retweets_of:nim_lang) lang:en -malware -virus -is:retweet -is:reply", {"max_results": "50"}.newStringTable).body.parseJson()["data"]
 
   echo "Found the following tweets: "
   echo data.pretty()
